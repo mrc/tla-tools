760f724a152a0bd2aaba1a7dfd1296f5e40d54c3
Matt Curtis <matt.r.curtis@gmail.com>
2019-05-09 02:11:18 +1000

    Added indent support for PlusCal blocks.
    Still lots to do, but blocks like begin/end align nicely, as do
    if/then/else.
    
    There is no support yet for aligning TLA+ logic operators.
    
    This patch also fixes the syntax table to support comments.

--- tla-pcal-mode.el
+++ tla-pcal-mode.el
@@ -30,16 +30,21 @@
 
 (defvar tla-mode-syntax-table
   (let ((table (make-syntax-table)))
+    (modify-syntax-entry ?\( "()1" table)  ; (* comment starter
+    (modify-syntax-entry ?\) ")(4" table)  ; *) comment ender
+    (modify-syntax-entry ?. "." table)
+    (modify-syntax-entry ?, "." table)
+    (modify-syntax-entry ?\\ ". 1" table)  ; \* comment starter
+    (modify-syntax-entry ?* ". 23c" table) ; (* or *) or \*
+    (modify-syntax-entry ?\n "> c" table)  ; \* comment ender
+    (modify-syntax-entry ?_ "w" table)     ; x_y
+    (modify-syntax-entry ?' "w" table)     ; x'
     table))
 
 (defvar pcal-mode-syntax-table
-  (let ((table (make-syntax-table)))
-    (modify-syntax-entry ?\( "()" table) ;; also need to handle (* .. *) comments
-    (modify-syntax-entry ?\) ")(" table)
+  (let ((table (copy-syntax-table tla-mode-syntax-table)))
     (modify-syntax-entry ?\{ "(}" table)
     (modify-syntax-entry ?\} "){" table)
-    (modify-syntax-entry ?. "." table)
-    (modify-syntax-entry ?, "." table)
     table))
 
 (defvar tla-mode-font-lock-keywords
@@ -80,7 +85,7 @@
     ))
 
 (defun tla-pcal-mode-set-comment-syntax-vars ()
-  (setq-local comment-start "\\\\\\*")
+  (setq-local comment-start "\\*")
   (setq-local comment-end "")
   (setq-local comment-use-syntax t)
   (setq-local comment-start-skip "\\(\\\\\\*\\)\\s *"))
@@ -100,14 +105,150 @@
   (setq-local indent-line-function #'pcal-mode-indent-line)
   )
 
+(defvar tla-mode-indent-offset 2
+  "Indent lines by this many columns.")
+
+(defvar tla-mode--conj-re "/\\\\") ; /\
+(defvar tla-mode--disj-re "\\\\/") ; \/
+(defvar tla-mode--neg1-re "#")     ; #
+(defvar tla-mode--neg2-re "/=")    ; /=
+(defvar tla-mode--impl-re "=>")    ; =>
+
+(defvar tla-mode--align-syntax-re
+  (concat tla-mode--conj-re "\\|"
+	  tla-mode--disj-re "\\|"
+	  tla-mode--neg1-re "\\|"
+	  tla-mode--neg2-re "\\|"
+	  tla-mode--impl-re)
+  "Regexp for matching TLA+ syntax to align.")
+
+(defvar pcal-mode-indent-offset 2
+  "Indent lines by this many columns.")
+
+(defvar pcal-mode--block-begin-re
+  (concat ".*\\<begin\\>"            "\\|"
+	  ".*\\<if\\>.*\\<then\\>"   "\\|"
+	  ".*\\<do\\>"               "\\|"
+	  ".*\\<either\\>")
+  "Regexp for matching the beginning of a block.")
+
+(defvar pcal-mode--block-else-re
+  ".*\\<else\\>"
+  "Regexp for matching the else condition of an if-then-else.")
+
+(defvar pcal-mode--block-end-re
+  (concat ".*\\<end\\>"              "\\|"
+	  ".*\\<end if\\>"           "\\|"
+	  ".*\\<end either\\>")
+  "Regexp for matching the end of a block.")
+
+(defun tla-mode--indent-column ()
+  "Find the appropriate column for the current line."
+  ;; Really basic: look at line above, if it has tla syntax, align
+  ;; with that. Doesn't handle indenting/outdenting operators that are
+  ;; not alike, which is a huge pain.
+  (forward-line -1)
+  (save-match-data
+    (if (looking-at (concat ".*\\(" tla-mode--align-syntax-re "\\)"))
+	(progn
+	  (goto-char (nth 2 (match-data)))
+	  ;; todo -- could return list of options for in/outdent?
+	  (current-column))
+      0))
+  ;; just return nil to skip indenting because it's annoying right now :-(
+  nil)
+
+(defvar pcal-mode--align-syntax-re
+  tla-mode--align-syntax-re
+  "Regexp for matching TLA+ syntax to align.")
+
+(defun pcal-mode--block-start ()
+  "Move to the start of the block."
+  (beginning-of-line)
+  (let ((level 0))
+    (while (not (or (bobp) (< level 0)))
+      (forward-line -1)
+      (cond ((looking-at-p pcal-mode--block-end-re)
+	     (setq level (+ level 1)))
+	    ((looking-at-p pcal-mode--block-begin-re)
+	     (setq level (- level 1))))))
+  (point))
+
 (defun pcal-mode--indent-column ()
-  ;; woefully inadequate, doesn't handle TLA+ rules
-  (car (syntax-ppss)))
+  "Find the appropriate column for the current line, or
+nil if the syntax isn't recognized for indentation."
+  (save-excursion
+    (beginning-of-line)
+    (cond ((bobp) 0)
+	  ((looking-at-p pcal-mode--block-end-re)
+	   (pcal-mode--block-start)
+	   (current-indentation))
+	  ((looking-at-p pcal-mode--block-else-re)
+	   (pcal-mode--block-start)
+	   (current-indentation))
+	  ((looking-at-p (concat "[[:blank:]]*" pcal-mode--align-syntax-re))
+	   (tla-mode--indent-column))
+	  (t
+	   ;; work backwards and base indent off the previous line
+	   (let (current)
+	     (while (null current)
+	       (forward-line -1)
+	       (cond ((looking-at-p pcal-mode--block-end-re)
+		      (setq current (current-indentation)))
+		     ((or (looking-at-p pcal-mode--block-begin-re)
+			  (looking-at-p pcal-mode--block-else-re))
+		      (setq current (+ (current-indentation) pcal-mode-indent-offset)))
+		     ((bobp)
+		      (setq current 0))))
+	     (and current (max 0 current)))))))
 
 (defun pcal-mode-indent-line ()
+  "Indent the current line according to PlusCal rules."
   (interactive)
-  (beginning-of-line)
-  (indent-line-to (pcal-mode--indent-column)))
+  (save-excursion
+    (beginning-of-line)
+    (let ((col (pcal-mode--indent-column)))
+      (when col
+	(indent-line-to col)))))
+
+;; polymode is doing a special indent for the first line, maybe it
+;; expects it's lining up markdown or something, but it's no good
+;; here. Can maybe fix it with advice around pm--first-line-indent --
+;; this uses some undocumented polymode stuff.
+(defun tla-pcal-mode--dont-indent-first-line (orig-fun &rest args)
+  (let ((res (apply orig-fun args)))
+    (if (and polymode-mode ; nb: true for both host and inner mode
+	     (eq major-mode 'pcal-mode))
+	0
+      res)))
+(advice-add 'pm--first-line-indent :around #'tla-pcal-mode--dont-indent-first-line)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; test indentation
+(require 'ert)
+(ert-deftest pcal-mode--indent-column-tests ()
+  (let ((lines '((0 "begin")
+		 (2 "  skip")
+		 (2 "  begin")
+		 (4 "    if blah then")
+		 (6 "      skip")
+		 (4 "    else")
+		 (6 "      skip")
+		 (4 "    end")
+		 (4 "    skip")
+		 (2 "  end")
+		 (0 "end"))))
+    (dotimes (n 100)
+      (with-temp-buffer
+	(dolist (line lines)
+	  (insert (cadr line) "\n"))
+	(goto-char (random (point-max)))
+	(save-excursion
+	  (beginning-of-line)
+	  (delete-horizontal-space))
+	(let ((res (pcal-mode--indent-column))
+	      (exp (car (elt lines (- (line-number-at-pos) 1)))))
+	  (should (= exp res)))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; polymode
@@ -116,8 +257,8 @@
 
 (define-innermode poly-tla-pcal--pcal-innermode
   :mode 'pcal-mode
-  :head-matcher "^(\\* --algorithm"
-  :tail-matcher "^end algorithm; \\*)$"
+  :head-matcher "^(\\*[[:blank:]]*--algorithm"
+  :tail-matcher "^[[:blank:]]*end[[:blank:]]+algorithm;[[:blank:]]*\\*)$"
   :head-mode 'host
   :tail-mode 'host)
 
