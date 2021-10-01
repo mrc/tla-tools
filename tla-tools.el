5ea42f48c9331dfac37280caa529dddd14472bfc
Carl Thuringer <carl@thuringer.us>
2021-10-01 09:08:16 -0500

    Add package.el dependency on polymode

--- tla-tools.el
+++ tla-tools.el
@@ -4,6 +4,7 @@
 
 ;; Author: Matt Curtis <matt.r.curtis@gmail.com>
 ;; Keywords: tools, languages
+;; Package-Requires: ((polymode "0.2.2"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
